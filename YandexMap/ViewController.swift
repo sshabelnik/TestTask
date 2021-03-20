9fbc509bf98e13ba094c9c5636dc8a032b2db2d6
sshabelnik <s.shabelnik01@gmail.com>
2021-03-20 16:44:01 +0300

    empty project

---
+++ YandexMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexMap
+//
+//  Created by Сергей Шабельник on 20.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
